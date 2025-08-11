fa8ed959f001e553c0ecc03e9512867afd5ba190
Emmanuel <emmanuelegwu41@gmail.com>
2025-08-11 10:15:33 +0100

    WIP, Day 2.2 Advent Of Code

--- Source/AOC24_DayTwo/AOC24_DayTwoPointTwo.py
+++ Source/AOC24_DayTwo/AOC24_DayTwoPointTwo.py
@@ -1,5 +1,5 @@
 def main():
-    with open(r"C:\Users\Emmanuel\Documents\PythonCode\AdventOfCode_2024\Source\AOC24_DayTwo\puzzle_input.txt", "r", encoding="utf-8") as puzzle_input:
+    with open(r"/home/tarantula/Documents/AdventOfCodeRepo/AdventOfCode_2024/Source/AOC24_DayTwo/puzzle_input.txt", "r", encoding="utf-8") as puzzle_input:
         number_of_safe_reports = 0
         while True:
             puzzle_list = puzzle_input.readline()
@@ -7,53 +7,85 @@ def main():
                 break
             puzzle_list = puzzle_list.strip().split()
             #print(puzzle_list)
-            if(check_safety(puzzle_list)):
+            is_safe = check_safety(puzzle_list)
+            if(is_safe == True):
                 number_of_safe_reports += 1
         print(number_of_safe_reports)
 
-def check_safety(puzzle_list=None):
+def check_safety(puzzle_list: list[str]) -> bool:
+    """
+    """
     levels = list(map(int, puzzle_list))
     safety_count = 0
     safety_count_max =1
-    if (levels == sorted(levels) or levels == sorted(list(map(int, levels)), reverse=True)):
-
-        previous_int = int(puzzle_list[0])
-        if any(i == 0 for i in puzzle_list):
-            return False
-        
-        for i in range(1, len(puzzle_list)):
-            if( abs(int(puzzle_list[i]) - previous_int) > 3 or  abs(int(puzzle_list[i]) - previous_int) < 1):
-                safety_count += 1
-
-            previous_int = int(puzzle_list[i])
-        print(puzzle_list)
-
+    if (levels == sorted(levels) or levels == sorted(levels, reverse=True)):
+        duplicates = find_duplicates(levels)
+        if not duplicates:
+            if(check_sorted_list_is_safe(levels)):
+                print (levels)
+                print("needs no modification")
+                return True
+            else:
+                for i in levels:
+                    result = remove_copy_list_item(i, levels)
+                    if(check_sorted_list_is_safe(levels)):
+                        print(result)
+                        print("removing an item makes it self")
+                        return True
+                    else: return False
+        else:
+            for i in range(len(levels)):
+                    result = remove_copy_list_item(i, levels)
+                    if(check_sorted_list_is_safe(levels)):
+                        print(result)
+                        print("duplicated but is safe after reomving one level")
+                        return True
+                    else: return False
     else:
-        previous_int = int(levels[0])
-        
-        for i in range(1, len(levels)):
-            check_list = levels
-            check_list.pop(i)
-            if (sorted(check_list)):
-                print(check_list)
-                print(levels)
-                print("Compare")
-                previous_int = int(puzzle_list[0])
-                if any(i == 0 for i in puzzle_list):
-                    return False
-        
-        for i in range(1, len(puzzle_list)):
-            if( abs(int(puzzle_list[i]) - previous_int) > 3 or  abs(int(puzzle_list[i]) - previous_int) < 1):
-                safety_count += 1
+        return False
 
-            previous_int = int(puzzle_list[i])
-        print(puzzle_list)
-    if(safety_count <= 1):
-        return True
         
  
+def remove_copy_list_item(index: int, unsafeList: list[int]) -> list[int]:
+    """
+    Remove duplicate value or value that might lead to unsafe condition from list
+    """
+    copy_list = unsafeList
+    copy_list.pop(index)
+    return copy_list
 
+def find_duplicates(input_list: list[int]) -> dict[int, int]:
+    """
+    find out if list contains duplicate of a value and return the integer that's duplicated alongside its duplicate indexes
+    """
+    duplicate_dict = {}
+    for i in range((len(input_list))):
+        _count = 0
+        #if(not input_list[i + 1]):   break
+        for j in range(len(input_list)):
+            if(input_list[i] == input_list[j]):
+                _count += 1
         
+        if (_count > 1):   
+            entry = {i : _count}
+            print(entry)
+            duplicate_dict.update(entry)
+    
+     
+    return duplicate_dict or None
+        
+def check_sorted_list_is_safe(puzzle_list: list[int]) -> bool:
+    """
+    """
+    previous_int = int(puzzle_list[0])
+    if any(i == 0 for i in puzzle_list):
+        return False
+    
+    for i in range(1, len(puzzle_list)):
+            if( abs(int(puzzle_list[i]) - previous_int) > 3 or  abs(int(puzzle_list[i]) - previous_int) < 1):
+                return True
+
+
 
 if __name__ == "__main__":
     main()
