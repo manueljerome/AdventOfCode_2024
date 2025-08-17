d2218d0137aa83a7a3690d0e5f236bdf50844d7a
Emmanuel Egwu <60505064+manueljerome@users.noreply.github.com>
2025-08-17 23:26:13 +0100

    Day 2.2 Advent of code completed

--- Source/AOC24_DayTwo/AOC24_DayTwoPointTwo.py
+++ Source/AOC24_DayTwo/AOC24_DayTwoPointTwo.py
@@ -1,123 +1,49 @@
-
-#"C:\Users\Emmanuel\Documents\PythonCode\AdventOfCode_2024\Source\AOC24_DayTwo\puzzle_input.txt"
-#"/home/tarantula/Documents/AdventOfCodeRepo/AdventOfCode_2024/Source/AOC24_DayTwo/puzzle_input.txt
 def main():
-    with open(r"/home/tarantula/Documents/AdventOfCodeRepo/AdventOfCode_2024/Source/AOC24_DayTwo/puzzle_input.txt", "r", encoding="utf-8") as puzzle_input:
+    with open(r"C:\Users\Emmanuel\Documents\PythonCode\AdventOfCode_2024\Source\AOC24_DayTwo\puzzle_input.txt", "r", encoding="utf-8") as puzzle_input:
         number_of_safe_reports = 0
         while True:
             puzzle_list = puzzle_input.readline()
             if not puzzle_list:
                 break
-            puzzle_list = puzzle_list.strip().split()
+            puzzle_list = list(map(int, puzzle_list.strip().split()))
             #print(puzzle_list)
-            is_safe = check_safety(puzzle_list)
-            if(is_safe == True):
+            if(check_safety(puzzle_list)):
                 number_of_safe_reports += 1
+            else:
+                print(puzzle_list)
+                for i in range(len(puzzle_list)): #for all the elements of this list in range of the length of the list minus one
+                    print(i)
+                    new_list = remove_copy_list_item(i, puzzle_list.copy()) #new list is equal to the new list after removing the element at the index 'i'
+                    if check_safety(new_list): 
+                        number_of_safe_reports += 1
+                        break
+                    
         print(number_of_safe_reports)
 
-def check_safety(puzzle_list: list[str]) -> bool:
+def check_safety(puzzle_list=None):
     """
+    Check that puzzle list adheres to the rules: adjacent integer mst differ by 1, 2 or 3
     """
+    levels = puzzle_list
+    if (levels == sorted(levels) or levels == sorted(list(map(int, levels)), reverse=True)):
+        previous_int = int(puzzle_list[0])
+        if any(i == 0 for i in puzzle_list):
+            return False
+        for i in range(1, len(puzzle_list)):
+            if( abs(int(puzzle_list[i]) - previous_int) > 3 or  abs(int(puzzle_list[i]) - previous_int) < 1):
+                return False
+            previous_int = int(puzzle_list[i])
+        print(puzzle_list)
+        return True
 
-    from collections import Counter
-    from itertools import chain
-
-    levels = list(map(int, puzzle_list))
-    safety_count = 0
-    safety_count_max =1
-    s = open(r"/home/tarantula/Documents/AdventOfCodeRepo/AdventOfCode_2024/Source/AOC24_DayTwo/puzzle_input.txt", "r", encoding="utf-8")
-    s_list = s.read()
-    if (levels == sorted(levels) or levels == sorted(levels, reverse=True)):
-        duplicates = find_duplicates(levels)
-        counts = Counter(chain.from_iterable(s))   # flatten then count
-        print({k: v for k, v in counts.items() if v > 1})
-
-
-        #print(duplicates)
-        if len(duplicates) == 0:
-            if(check_sorted_list_is_safe(levels)):
-                #print (levels)
-                #print("needs no modification")
-                return True
-            else:
-                for i in range(len(levels)):
-                    result = remove_copy_list_item(i, levels)
-                    if(check_sorted_list_is_safe(result)):
-                        #print(levels)
-                        #print("removing an item makes it safe")
-                        return True
-                    else: return False
-        else:
-            for i in range(len(levels)):
-                    result = remove_copy_list_item(i, levels)
-                    if(check_sorted_list_is_safe(result)):
-                        #print(levels)
-                        #print("duplicated but is safe after removing one level")
-                        return True
-                    else: return False
-    else:
-        for i in range(len(levels)):
-                    result = remove_copy_list_item(i, levels)
-                    if(check_sorted_list_is_safe(result)):
-                        #print(levels)
-                        #print("removing an item makes it safe")
-                        return True
-                    else: return False
-
-        
- 
 def remove_copy_list_item(index: int, unsafeList: list[int]) -> list[int]:
     """
     Remove duplicate value or value that might lead to unsafe condition from list
     """
 
-    copy_list = unsafeList
-    copy_list.pop(index)
-    return copy_list
-
-def find_duplicates(input_list: list[int]) -> list[int]:
-    """
-    find out if list contains duplicate of a value and return the integer that's duplicated alongside its duplicate indexes
-    """
-    from collections import Counter
-
-    counts = Counter(input_list)
-    duplicate_dict = {k: v for k, v in counts.items() if v > 1}
-    #print (counts)
-    duplicated_numbers_list = list(duplicate_dict)
-    
-    #print(duplicate_dict)
-    # for i in range((len(input_list))):
-    #     _count = 0
-    #     if input_list[i] in duplicated_numbers_list: continue
-    #     for j in range(len(input_list)):
-    #         if(input_list[i] == input_list[j]):
-    #             _count += 1
-                
-                
-        
-    #     if (_count > 1):  
-    #         duplicated_numbers_list.append(input_list[i]) 
-    #         #print (duplicated_numbers_list)
-    #         entry = {input_list[i] : _count}
-    #         duplicate_dict.update(entry) 
-    return duplicated_numbers_list
-        
-        
-def check_sorted_list_is_safe(puzzle_list: list[int]) -> bool:
-    """
-    """
-    previous_int = puzzle_list[0]
-    if any(i == 0 for i in puzzle_list):
-        return False
-    
-    for i in range(1, len(puzzle_list)):
-            if(abs(int(puzzle_list[i]) - previous_int) > 3 or  abs(int(puzzle_list[i]) - previous_int) < 1):
-                return False
-            previous_int = puzzle_list[i]
-    return True
-
-
+    copy_list = unsafeList #assign the list sent to a copy
+    copy_list.pop(index)#remove the element at this index
+    return copy_list#return the new list
 
 if __name__ == "__main__":
     main()
